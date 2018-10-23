7b92aeabe6342de736101e5200b02f24e15513e8
ninja <govindrajan566@gmail.com>
2018-10-23 11:14:41 +0530

    first commit

---
+++ src/components/startPage.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import '../assets/css/cars.css'
+
+export default class StartPage extends React.Component {
+    constructor(props, context) {
+        super(props, context)
+
+        this.state = {
+            // your state items here
+        }
+    }
+
+    render () {
+        return (
+            // your code here
+
+            // delete
+            <h1>Hello guys, welcome to TScaleHub</h1>
+            // delete
+        )
+    }
+}
